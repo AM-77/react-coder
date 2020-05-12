ac7f4823fb7cbf49323861a302aa7b1169d4eadf
AM-77 <amine.griche77@gmail.com>
2020-05-12 15:57:01 +0200

    added line numbers

--- example/src/Coder/Highliter.tsx
+++ example/src/Coder/Highliter.tsx
@@ -1,31 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import Prism from "prismjs"
+import 'prismjs/plugins/line-numbers/prism-line-numbers'
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 
 interface IProps {
-  code: string
+  code: string,
+  lineNumbers: boolean,
+  language: string,
+  fontSize?: number
 }
 
 export default class Highliter extends Component<IProps> {
 
-  componentDidMount() {
-    Prism.highlightAll()
-  }
+  private ref : React.RefObject<HTMLElement> = createRef() 
+
+  componentDidMount = () => this.highlightCode() 
+
+  componentDidUpdate = () => this.highlightCode() 
+  
+  highlightCode = () => (this.ref && this.ref.current) && Prism.highlightElement(this.ref.current)
 
   render() {
-    const { code } = this.props
+    const { code, lineNumbers, language, fontSize = 12 } = this.props
     return (
-      <div className="coder-container">
-        <div className="numbers">
-          {
-            (code.match(/\n/g) || []).map((line, index) => <span className="number" key={index}>{ index + 1 }</span>)
-          }
-        </div>
-        <pre className="line-numbers">
-          <code className="language-js">
-            {code}
-          </code>
-        </pre>
-      </div>
+      <pre className={lineNumbers ? "line-numbers" : ""} style={{ fontSize }}>
+        <code ref={this.ref} className={`language-${language}`}>
+          { code.trim() } 
+        </code>
+      </pre>
     )
   }
+
 }
