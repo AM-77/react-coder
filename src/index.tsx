9127e01ad91f2760988e5c9367e6daafed67c282
AM-77 <amine.griche77@gmail.com>
2020-05-12 15:55:24 +0200

    fix set up

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,39 @@
 import * as React from 'react'
-import styles from './styles.module.css'
+// import styles from './styles.module.css'
+import Prism from "prismjs"
 
-interface Props {
-  text: string
+const code = `
+const foo = 'foo';
+const bar = 'bar';
+console.log(foo + bar);
+const foo = 'foo';
+const bar = 'bar';
+console.log(foo + bar);
+const foo = 'foo';
+const bar = 'bar';
+console.log(foo + bar);
+`.trim()
+
+interface IProps {
+  langauge: string
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export default class Coder extends React.Component<IProps> {
+  componentDidMount() {
+    // You can call the Prism.js API here
+    // Use setTimeout to push onto callback queue so it runs after the DOM is updated
+    setTimeout(()=>{
+      Prism.highlightAll()
+    }, 10)
+  }
+  render() {
+    // const { langauge } = this.props
+    return (
+      <pre className="line-numbers">
+        <code className="langauge-js">
+          {code}
+        </code>
+      </pre>
+    )
+  }
 }
