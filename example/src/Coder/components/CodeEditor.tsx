2b4768857f2bf9b55492897704f026c15af7b04d
AM-77 <amine.griche77@gmail.com>
2020-05-13 15:43:35 +0200

    added Single Line Tab support

--- example/src/Coder/components/CodeEditor.tsx
+++ example/src/Coder/components/CodeEditor.tsx
@@ -30,7 +30,7 @@ export default class CodeEditor extends Component<IProps, IState> {
   onChange = (e: React.ChangeEvent<HTMLTextAreaElement>):void => {
     const { onCodeChange } = this.props
     let value = e.target.value
-    this.setState({code: value})
+    this.setState({ code: value })
     onCodeChange(value)
   }
 
@@ -54,9 +54,35 @@ export default class CodeEditor extends Component<IProps, IState> {
   }
 
   onKeyDown = (e: React.KeyboardEvent) => {
-    const { readOnly } = this.props
+    const { readOnly, onCodeChange } = this.props
     if (readOnly) { 
       e.preventDefault() 
+    } else if (e.key === 'Tab') {
+      e.preventDefault();
+      const value = this.textareaRef.current!.value;
+      const selectionStart = this.textareaRef.current!.selectionStart;
+      const selectionEnd = this.textareaRef.current!.selectionEnd;
+
+      if (!e.shiftKey) {
+        const code = value.substring(0, selectionStart) + '  ' + value.substring(selectionEnd)
+        this.setState({ code }, () => {
+          this.textareaRef.current!.selectionStart = selectionEnd + 2 - (selectionEnd - selectionStart);
+          this.textareaRef.current!.selectionEnd = selectionEnd + 2 - (selectionEnd - selectionStart);
+        })
+        onCodeChange(code)
+      } else {
+        const beforeStart = value.substring(0, selectionStart).split('').reverse().join('')
+        const indexOfTab = beforeStart.indexOf('  ')
+        const indexOfNewLine = beforeStart.indexOf('\n') 
+        const code = beforeStart.substring(indexOfTab + 2).split('').reverse().join('') + beforeStart.substring(0, indexOfTab).split('').reverse().join('') + value.substring(selectionEnd)
+        if (indexOfTab !== -1 && (indexOfTab < indexOfNewLine || indexOfNewLine === -1)) {
+          this.setState({ code }, () => {
+            this.textareaRef.current!.selectionStart = selectionEnd - 2 - (selectionEnd - selectionStart);
+            this.textareaRef.current!.selectionEnd = selectionEnd - 2 - (selectionEnd - selectionStart);
+          })
+          onCodeChange(code)
+        }
+      }
     }
   }
 
