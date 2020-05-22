ee5b26b519b174f531a817d82fb8ae9effa7a3f0
AM-77 <amine.griche77@gmail.com>
2020-05-22 12:44:14 +0200

    fix styles

--- src/components/Highliter.tsx
+++ src/components/Highliter.tsx
@@ -1,7 +1,5 @@
 import React, { Component, createRef } from 'react'
 import Prism from "prismjs"
-import 'prismjs/plugins/line-numbers/prism-line-numbers'
-import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 
 interface IProps {
   code: string,
@@ -22,21 +20,43 @@ export default class Highliter extends Component<IProps> {
 
   render() {
     const { code, lineNumbers, language, fontSize = 12 } = this.props
-
     
     return (
       <pre className={lineNumbers ? "line-numbers" : ""} 
           style={{ 
-            fontSize, margin: "0", 
+            fontSize, 
+            margin: "0", 
             boxSizing: "border-box", 
             background: "transparent",
             whiteSpace: "pre-wrap",
-            wordWrap: "break-word", }}>
+            wordWrap: "normal",
+            paddingTop: "1em",
+            paddingRight: "1em",
+            paddingBottom: "1em", 
+            lineHeight: "1.5",
+            position: "absolute", 
+            top: "0px", 
+            left: "0px", 
+            maxWidth: "100%", 
+            minWidth: "100%", 
+            border: "none", 
+            fontWeight: "normal", 
+            textAlign: "left", 
+            wordSpacing: "normal", 
+            wordBreak: "break-word", 
+            overflowWrap: "normal",  
+            tabSize: "4", 
+            hyphens: "none", 
+            cursor: "auto", 
+            resize: "none", 
+            overflowY: "hidden"}}>
         
         <code ref={this.ref} style={{ 
             whiteSpace: "pre-wrap",
-            wordWrap: "break-word",
-            fontFamily: "'Fira Code', Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace" }} 
+            wordWrap: "normal",
+            overflowWrap: "normal",
+            fontFamily: "'Fira Code', Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
+            fontWeight: "normal"}}
             className={`language-${language}`}>
 
           { code } 
